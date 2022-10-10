b7dd332b8a5ed191795dbd8e2b59ff6ca423ead3
Shakil Ahmed Atik <shakilatik@gmail.com>
2022-10-10 08:09:58 +0600

    header added

--- Restart/Screens/homeView.swift
+++ Restart/Screens/homeView.swift
@@ -10,15 +10,41 @@ import SwiftUI
 struct HomeView: View {
     @AppStorage("onboarding") var isOnboardingViewActive : Bool = false
     var body: some View {
-        VStack(spacing: 20) {
-            Text("Home")
-                .font(.largeTitle)
-            Button(action:{
-               isOnboardingViewActive = true
-            }) {
-                Text("Retart")
-            }
-        }//:VSTACK
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
+            VStack(spacing: 20) {
+                // MARK: - HEADER
+                
+                Spacer()
+                
+                VStack(spacing: 0){
+                    Text("Share.")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                    
+                    Text("""
+                    It's not how much we give
+                    but how much love we put into giving.
+                    """)
+                        .font(.title3)
+                        .fontWeight(.light)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal, 10)
+                    
+                    
+                }//: HEADER
+                
+                
+                // MARK: - CENTER
+                
+                
+                // MARK: - FOOTER
+               
+            }//:VSTACK
+        }//:ZSTACK
     }
 }
 
