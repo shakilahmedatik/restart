33a51609291c8216aecac21ed63f154577d4e8fb
Shakil Ahmed Atik <shakilatik@gmail.com>
2022-10-10 17:35:49 +0600

    custom button added

--- Restart/Screens/homeView.swift
+++ Restart/Screens/homeView.swift
@@ -15,7 +15,7 @@ struct HomeView: View {
                 .ignoresSafeArea(.all, edges: .all)
             VStack(spacing: 20) {
                 // MARK: - HEADER
-                
+            
                 Spacer()
                 
                 VStack(spacing: 0){
@@ -56,8 +56,56 @@ struct HomeView: View {
                 }//: CENTER
                 
                 Spacer()
+                
                 // MARK: - FOOTER
-               
+                
+                ZStack{
+                    // PARTS OF THE CUSTOM BUTTON
+                    
+                    // 1. BACKGROUND (STATIC)
+                    Capsule()
+                        .fill(.white.opacity(0.2))
+                    Capsule()
+                        .fill(.white.opacity(0.2))
+                        .padding(8)
+                    // 2. CALL-TO-ACTION (STATIC)
+                    Text("Get Started")
+                        .font(.system(.title, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x: 20)
+                    // 3. CAPSULE (DYNAMIC WIDTH)
+                    HStack{
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: 80)
+                        
+                        Spacer()
+                    }
+                    // 4. CIRCLE (DRAGGABLE)
+                    HStack {
+                        ZStack{
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                            
+                        }
+                        .foregroundColor(.white)
+                    .frame(width: 80,height: 80,alignment: .center)
+                    .onTapGesture{
+                        isOnboardingViewActive = true
+                    }
+                        Spacer()
+                    }
+                    
+                    
+                }//: FOOTER
+                .frame(height: 80, alignment: .center)
+                .padding()
             }//:VSTACK
         }//:ZSTACK
     }
