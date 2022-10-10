22a5bd36296c14d5b47ba11ddec88d2398f176f1
Shakil Ahmed Atik <shakilatik@gmail.com>
2022-10-10 06:45:09 +0600

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Atik on 10/10/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
