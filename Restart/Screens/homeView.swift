13ae47337af3a512e0d490ec7a539d81fd96f87d
Shakil Ahmed Atik <shakilatik@gmail.com>
2022-10-11 14:48:24 +0600

    header added in home view

--- Restart/Screens/homeView.swift
+++ Restart/Screens/homeView.swift
@@ -8,106 +8,34 @@
 import SwiftUI
 
 struct HomeView: View {
+    
     @AppStorage("onboarding") var isOnboardingViewActive : Bool = false
+    
     var body: some View {
-        ZStack {
-            Color("ColorBlue")
-                .ignoresSafeArea(.all, edges: .all)
-            VStack(spacing: 20) {
-                // MARK: - HEADER
+        VStack(spacing:20) {
             
-                Spacer()
-                
-                VStack(spacing: 0){
-                    Text("Share.")
-                        .font(.system(size: 60))
-                        .fontWeight(.heavy)
-                        .foregroundColor(.white)
-                    
-                    Text("""
-                    It's not how much we give
-                    but how much love we put into giving.
-                    """)
-                        .font(.title3)
-                        .fontWeight(.light)
-                        .foregroundColor(.white)
-                        .multilineTextAlignment(.center)
-                        .padding(.horizontal, 10)
-                    
-                    
-                }//: HEADER
-                
-                
-                // MARK: - CENTER
-                ZStack{
-                    ZStack{
-                        Circle()
-                            .stroke(.white.opacity(0.2), lineWidth: 40)
-                            .frame(width: 260, height: 260, alignment: .center)
-                        Circle()
-                            .stroke(.white.opacity(0.2), lineWidth: 80)
-                            .frame(width: 260, height: 260, alignment: .center)
-                    }
-                    
-                    Image("character-1")
-                        .resizable()
-                        .scaledToFit()
-                    
-                }//: CENTER
-                
-                Spacer()
-                
-                // MARK: - FOOTER
-                
-                ZStack{
-                    // PARTS OF THE CUSTOM BUTTON
-                    
-                    // 1. BACKGROUND (STATIC)
-                    Capsule()
-                        .fill(.white.opacity(0.2))
-                    Capsule()
-                        .fill(.white.opacity(0.2))
-                        .padding(8)
-                    // 2. CALL-TO-ACTION (STATIC)
-                    Text("Get Started")
-                        .font(.system(.title, design: .rounded))
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
-                        .offset(x: 20)
-                    // 3. CAPSULE (DYNAMIC WIDTH)
-                    HStack{
-                        Capsule()
-                            .fill(Color("ColorRed"))
-                            .frame(width: 80)
-                        
-                        Spacer()
-                    }
-                    // 4. CIRCLE (DRAGGABLE)
-                    HStack {
-                        ZStack{
-                            Circle()
-                                .fill(Color("ColorRed"))
-                            Circle()
-                                .fill(.black.opacity(0.15))
-                                .padding(8)
-                            Image(systemName: "chevron.right.2")
-                                .font(.system(size: 24, weight: .bold))
-                            
-                        }
-                        .foregroundColor(.white)
-                    .frame(width: 80,height: 80,alignment: .center)
-                    .onTapGesture{
-                        isOnboardingViewActive = true
-                    }
-                        Spacer()
-                    }
-                    
-                    
-                }//: FOOTER
-                .frame(height: 80, alignment: .center)
+            // MARK: - HEADER
+            
+            Spacer()
+            
+            Image("character-2")
+                .resizable()
+                .scaledToFit()
                 .padding()
-            }//:VSTACK
-        }//:ZSTACK
+            
+            
+            // MARK: - CENTER
+            
+            // MARK: - FOOTER
+            
+            Button(action:{
+                isOnboardingViewActive = true
+            }) {
+                Text("Restart")
+            }//:BUTTON
+            
+         
+        }//:VSTACK
     }
 }
 
