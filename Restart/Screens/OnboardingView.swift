6d5f482f475fa45ff62735b0e391949952358371
Shakil Ahmed Atik <shakilatik@gmail.com>
2022-10-15 23:45:20 +0600

    image animation added

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -14,6 +14,7 @@ struct OnboardingView: View {
     @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
     @State private var buttonOffset: CGFloat = 0
     @State private var isAnimating: Bool = false
+    @State private var imageOffset: CGSize = .zero
     
     var body: some View {
         ZStack {
@@ -56,6 +57,14 @@ struct OnboardingView: View {
                         .scaledToFit()
                         .opacity(isAnimating ? 1 : 0)
                         .animation(.easeOut(duration: 0.5), value: isAnimating)
+                        .offset(x: imageOffset.width * 1.2, y:0)
+                        .gesture( DragGesture()
+                            .onChanged{ gesture in
+                                if abs(imageOffset.width) <= 150 {
+                                    imageOffset = gesture.translation
+                                }
+                                
+                            })
                 }//: CENTER
                
                 
