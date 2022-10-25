513d10825fb251ffc06d1ee55e62f4a2186192df
Shakil Ahmed Atik <shakilatik@gmail.com>
2022-10-25 14:08:27 +0600

    sound files added in btn actions

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -160,6 +160,7 @@ struct OnboardingView: View {
                             withAnimation(Animation.easeOut(duration: 0.4)) {
                                 
                                 if buttonOffset > buttonWidth / 2{
+                                    playSound(sound: "chimeup", type: "mp3")
                                     buttonOffset = buttonWidth - 80
                                     isOnboardingViewActive = false
                                     
